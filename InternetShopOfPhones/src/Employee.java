be0b5a974202d7eff2f40e852739a7d72e304253
akshin057 <145829826+akshin057@users.noreply.github.com>
2024-04-03 20:50:20 +0300

    Была добавлена функция toString в класс Employee

---
+++ InternetShopOfPhones/src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee extends Person{
+
+    public Employee(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
